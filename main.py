939efa1c147bbadcbc1676d5ac01e09c07473f0d
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2019-06-26 17:14:01 +0900

    写経なう

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from token import Token, TokenType
-
-if __name__ == '__main__':
-    number = Token(TokenType.NUMBER, "30")
-    print(f'Type->{number.type.string()}\tInput->{number.input}')
+from lex import lexng, Lexer
+import os
+import sys
