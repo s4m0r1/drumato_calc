bed7695d5416643f859ca2752af1879f281ff087
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2019-06-26 17:32:23 +0900

    動かなくなった死

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-from lex import lexng, Lexer
+from lex import lexing, Lexer
 import os
 import sys
+
+def get_contents_from_file(filename: str) -> str:
+    if not os.path.exists(filename):
+        print(
+            f"warning: no such file '{filename}'\nread from cmdarg instead of file..."
+        )
+        return filename + '\x00'
+    return open(filename, 'r').read()
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Fatal error: no input files")
+        sys.exit()
+    print("--------input--------")
+    contents = get_contents_from_file(sys.argv[1])
+    print(contents)
+    tokens = lexing(contents)
+    print("--------tokens--------")
+    [print(t.string()) for t in tokens]
